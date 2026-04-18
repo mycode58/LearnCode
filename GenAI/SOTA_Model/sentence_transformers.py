7798fb65d18fad5f45b53e8f942a58a76ee723e9
snapit <133040521+mycode58@users.noreply.github.com>
2026-04-18 23:21:11 +0530

    Initialize HF_TOKEN and add SentenceTransformer usage
    Set up environment variable for HF_TOKEN and added a sentence transformer model to generate embeddings.

--- GenAI/SOTA_Model/sentence_transformers.py
+++ GenAI/SOTA_Model/sentence_transformers.py
@@ -1,1 +1,14 @@
-print('ttt')
+import os
+#os.environ["GOOGLE_API_KEY"] =os.getenv("GOOGLE_API_KEY")
+hf_token = os.getenv("HF_TOKEN")
+if hf_token:
+    os.environ["HF_TOKEN"] = hf_token
+
+from sentence_transformers import SentenceTransformer
+
+open_source_embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+text = "machine"
+embedding = open_source_embedding_model.encode(text)
+print(embedding)
+print(len(embedding))
